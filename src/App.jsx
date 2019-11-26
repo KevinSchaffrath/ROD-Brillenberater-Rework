fdd0b99ff4bfdfa9bb63618d892c43f84d1d16fe
Kevin Schaffrath <k.schaffrath@nfq.de>
2019-11-26 14:54:02 +0100

    chore(Global): Integrated initial Settings from base-project

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, {PureComponent} from 'react';
+
+/**
+ * App Component.
+ *
+ * @export
+ * @class App
+ * @extends {PureComponent}
+ */
+export default class App extends PureComponent {
+    /**
+     * Renders the App.
+     *
+     * @returns {JSX} React Component.
+     * @memberof App
+     */
+    render() {
+        return (
+            <>
+                <AppContainer>
+                    <SmallText>
+                        Wenn dein Name nicht der richtige ist und dein Projekt
+                        auch nicht dann schau bitte in die README.MD!
+                    </SmallText>
+                </AppContainer>
+            </>
+        );
+    }
+}
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background: #202229;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const SmallText = styled.span`
+    color: #ff6700;
+    text-align: center;
+    font-family: Arial;
+`;
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const TerserJs = require('terser-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const webpack = require('webpack');
+const portfinder = require('portfinder');
+const outputDirectory = 'dist';
+
+module.exports = async (env, argv) => ({
+    entry: './src/main.jsx',
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: 'main.js'
+    },
+    resolve: {
+        alias: {
+            Fonts: path.resolve(__dirname, 'src/assets/fonts'),
+            Images: path.resolve(__dirname, 'src/assets/img'),
+            Icons: path.resolve(__dirname, 'src/assets/icons'),
+            Downloads: path.resolve(__dirname, 'src/assets/downloads'),
+            Screens: path.resolve(__dirname, 'src/screens'),
+            Components: path.resolve(__dirname, 'src/components'),
+            Utils: path.resolve(__dirname, 'src/utils'),
+            Theme: path.resolve(__dirname, 'src/theme'),
+            Stores: path.resolve(__dirname, 'src/stores')
+        },
+        extensions: [
+            '.js',
+            '.jsx',
+            '.png',
+            '.jpeg',
+            '.jpg',
+            '.json'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {loader: 'babel-loader'}
+            },
+            {
+                test: /\.(gif|png|jpg|woff|woff2|eot|ttf|svg)$/,
+                loader: 'file-loader?name=[path][name].[ext]'
+            },
+            {
+                test: /\.pdf$/,
+                use: 'file-loader?name=[path][name].[ext]'
+            }
+        ]
+    },
+    devServer: {
+        port: await getPort(4000), // eslint-disable-line
+        host: '127.0.0.1',
+        open: true,
+        disableHostCheck: true
+    },
+    devtool: 'source-map',
+    optimization: {
+        runtimeChunk: 'single',
+        minimizer: [new TerserJs({
+            test: /\.js(\?.*)?$/i,
+            parallel: true,
+            sourceMap: true,
+            terserOptions: {
+                warnings: false,
+                ie11: false,
+                mangle: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                deadCode: true,
+                evaluate: true,
+                ifReturn: true,
+                joinVars: true
+            }
+        })],
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.HashedModuleIdsPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/favicon.ico',
+        }),
+        new ScriptExtHtmlWebpackPlugin({defaultAttribute: 'defer'}),
+        new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(argv.mode)})
+    ]
+});
+
+/**
+ * Gets the nearest open port after the default Port given.
+ *
+ * @param {Number} defaultPort The Port you want to use.
+ * @returns {Number} The port nearest to the one you want and that is open.
+ */
+async function getPort(defaultPort) {
+    portfinder.basePort = defaultPort;
+
+    return portfinder.getPortPromise();
+}
